5e441ddc13eb9cc411f312bb980c052f3f9205a7
Anurag <kr.anurag24@gmail.com>
2023-02-28 14:13:18 +0530

    chore: clean up deps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,12 @@
 import "@/styles/globals.css";
 import { theme } from "@/styles/theme";
 import { ChakraProvider } from "@chakra-ui/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
-import "@fontsource/montserrat/900.css";
-import "@fontsource/poppins/400.css";
-import "@fontsource/poppins/500.css";
-import "@fontsource/poppins/600.css";
-import "@fontsource/poppins/700.css";
-import "@fontsource/poppins/800.css";
-import "@fontsource/poppins/900.css";
-
-const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </QueryClientProvider>
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
   );
 }
